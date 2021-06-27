e6706794476f1919cfce2fbe4a95a2253878df76
Jerson Estrada <jersonestrada201930199@cunoc.edu.gt>
2021-06-27 02:37:20 -0600

    Se agrego la portada del Juego

---
+++ src/principal/Main.java
@@ -0,0 +1,13 @@
+
+package principal;
+import inicioJuego.Inicio;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+       Inicio portada = new Inicio();
+    }
+    
+}
